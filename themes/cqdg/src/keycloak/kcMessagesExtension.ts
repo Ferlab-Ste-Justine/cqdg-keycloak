1384099980b75b84316e41006993433037b3cecb
Celine Pelletier <cpelletier@ferlab.bio>
2023-02-16 16:22:58 -0500

    feat: CQDG-38 Terms and Disclaimer page and some clean up

--- themes/cqdg/src/keycloak/kcMessagesExtension.ts
+++ themes/cqdg/src/keycloak/kcMessagesExtension.ts
@@ -4,7 +4,27 @@ import { kcMessages } from "keycloakify/lib/i18n/useKcMessage";
 Object.assign(
     kcMessages["en"],
     {
-        "loginTitle": "Log in with",
+        "login_title": "Log in with",
+        "registration_title": "CQDG Portal Registration",
+        "portal_introduction": "The CQDG data portal enables research, visualization and access to sensitive and confidential data. Some datasets may require additional approvals and terms and conditions from the studies trust.",
+        "terms_title": "CQDG Portal Terms & Conditions",
+        "last_update_tooltip": "Last updated on (yyyy-mm-dd)",
+        "terms_1": "You are responsible for maintaining the confidentiality of your account and all activities that take place as a result of the use of your account;",
+        "terms_2": "You are responsible for all material that you submit to this site. You must not transmit any content or documents that contain corrupted files, viruses or any other similar software file that could damage the functioning of a computer system. You must ensure that no personal information is included in the data submitted to CQDG;",
+        "terms_3": "In accordance with the Data Access and Use Policies of the studies, you must take appropriate measures to protect the data against theft, loss, interception, use or unauthorized disclosure;",
+        "terms_4": "You will use controlled access datasets only in connection with the research project described in an approved data access request for each dataset;",
+        "terms_5": "You adhere to the intellectual property clauses (if applicable) as specified in the Data Access and Use Policies of the studies;",
+        "terms_6": "You will not make any attempt to identify or contact any participants or groups of participants from whom the data was collected, or to generate any information that could easily determine the identity of the participants;",
+        "terms_7": "You will report any unauthorized disclosure of data, data security breach or other data management incidents that violate the Data Access Terms;",
+        "disclaimer_title": "CQDG Portal Disclaimers",
+        "disclaimer_1": "Data available in the CQDG Portal is provided on an AS-IS basis and may change over time.",
+        "disclaimer_2": "CQDG does not warrant or assume any legal liability or responsibility for information, apparatus, product, or process contained in the CQDG Portal.",
+        "disclaimer_3": "Content provided on the CQDG Portal is for informational purposes only and is not intended to be a substitute for independent professional medical judgment, advice, diagnosis, or treatment.",
+        "terms_checkbox": "I have read and agree to the CQDG Portal Terms and Conditions",
+        "disclaimer_checkbox": "I have read and understand the CQDG Portal Disclaimers",
+        "cancel": "Cancel",
+        "next": "Next",
+        "terms_disclaimer_error": "Please accept the terms & conditions and portal disclaimers"
     }
 );
 
@@ -12,6 +32,26 @@ Object.assign(
 Object.assign(
     kcMessages["fr"],
     {
-        "loginTitle": "Choisir votre identifiant",
+        "login_title": "Choisir votre identifiant",
+        "registration_title": "Inscription au portail du CQDG",
+        "portal_introduction": "Le portail de données du CQDG permet la recherche, la visualisation et l’accès à des données sensibles et confidentielles. Certains jeux de données peuvent exiger des autorisations et des termes et conditions supplémentaires de la part des études fiduciaires.",
+        "terms_title": "Termes & conditions d’utilisation du portail CQDG",
+        "last_update_tooltip": "Dernière mise à jour (aaaa-mm-jj)",
+        "terms_1": "Vous êtes responsable du maintien de la confidentialité de votre compte et de toutes les activités en lien avec l’utilisation de votre compte ;",
+        "terms_2": "Vous êtes responsable de tout le matériel que vous soumettez sur ce site. Vous ne devez pas transmettre du contenu ou des documents qui contiennent des fichiers corrompus, des virus ou tout autre fichier logiciel similaire qui puissent endommager le fonctionnement d’un système informatique. Vous devez vous assurer qu’aucun renseignement personnel ne soit inclus dans les données soumises au CQDG ;",
+        "terms_3": "Vous devez prendre les mesures appropriées pour protéger les données contre tout vol, perte, interception, utilisation ou divulgation non autorisée, conformément aux politiques d’accès aux données des études fiduciaires ;",
+        "terms_4": "Vous devez utiliser les ensembles de données à accès contrôlé uniquement pour les fins du projet de recherche approuvé ;",
+        "terms_5": "Vous devez adhérer aux clauses concernant la propriété intellectuelle (le cas échéant) telles que spécifiées dans les politiques d’accès aux données des études fiduciaires ;",
+        "terms_6": "Vous ne devez faire aucune tentative d’identifier ou de contacter des participants ou des groupes de participants auprès desquels les données ont été collectées, ou de générer des informations qui pourraient permettre d'identifier des participants ;",
+        "terms_7": "Vous devez rapporter toute divulgation non autorisée de données, violation de la sécurité des données ou autres incidents de gestion de données contraires aux conditions d'accès aux données ;",
+        "disclaimer_title": "Avis de non-responsabilité du portail CQDG",
+        "disclaimer_1": "Les données disponibles sur le portail CQDG sont fournies telles quelles et peuvent changer au fil du temps.",
+        "disclaimer_2": "Le CQDG ne garantit ni n'assume aucune responsabilité légale ou responsabilité pour les informations, appareils, produits ou processus contenus dans le portail CQDG.",
+        "disclaimer_3": "Le contenu fourni sur le portail CQDG est à titre informatif uniquement et n'est pas destiné à se substituer à un jugement, un conseil, un diagnostic ou un traitement d’un professionnel de la santé.",
+        "terms_checkbox": "J'ai lu et j'accepte les termes et conditions du portail CQDG",
+        "disclaimer_checkbox": "J'ai lu et compris les clauses de non-responsabilité du portail CQDG",
+        "cancel": "Annuler",
+        "next": "Suivant",
+        "terms_disclaimer_error": "Veuillez accepter les termes & conditions  et les clauses de non-responsabilité du portail"
     }
 );
--- themes/cqdg/src/views/Registration/Terms/index.tsx
+++ themes/cqdg/src/views/Registration/Terms/index.tsx
@@ -1,7 +1,8 @@
-import { ArrowRightOutlined } from "@ant-design/icons";
+import { ArrowRightOutlined, InfoCircleOutlined } from "@ant-design/icons";
 import ScrollContent from "@ferlab/ui/core/layout/ScrollContent";
 import GridCard from "@ferlab/ui/core/view/v2/GridCard";
-import { Button, Checkbox, Form, Space, Typography } from "antd";
+import { Button, Checkbox, Form, Space, Tooltip, Typography } from "antd";
+import { useKcMessage } from "keycloakify";
 import { useDispatch } from "react-redux";
 import { useRegistrationFlow } from "store/registrationFlow";
 import { registrationFlowActions } from "store/registrationFlow/slice";
@@ -9,6 +10,8 @@ import { TUser } from "store/registrationFlow/types";
 import { KcContext_LoginUpdateProfile } from "..";
 
 import styles from "./index.module.scss";
+
+const LAST_UPDATED_DATE = "2023-02-16";
 
 enum FORM_FIELDS {
   CHECKBOXES = "checkboxes",
@@ -26,9 +29,13 @@ const TermsStep = ({
 }: {
   kcContext: KcContext_LoginUpdateProfile;
 }) => {
+  const { redirectUrl } = kcContext;
+
   const dispatch = useDispatch();
   const [form] = Form.useForm();
   const { userInfo } = useRegistrationFlow();
+
+  const { advancedMsg } = useKcMessage();
 
   const getInitialValues = (localUser: TUser) => {
     let value = [];
@@ -50,13 +57,10 @@ const TermsStep = ({
       className={styles.termsAndConditionsWrapper}
     >
       <Title level={3} className={styles.mainTitle}>
-        INCLUDE Portal Registration Process
+        {advancedMsg("registration_title")}
       </Title>
       <Text>
-        The INCLUDE Portal is the primary entry point to the INCLUDE Data Hub.
-        The INCLUDE Portal enables searching, visualizing, and accessing
-        INCLUDE-relevant data. Some datasets may require additional approvals
-        (e.g., dbGaP) and terms and conditions of access and use.
+        {advancedMsg("portal_introduction")}
       </Text>
       <GridCard
         wrapperClassName={styles.cardWrapper}
@@ -64,86 +68,39 @@ const TermsStep = ({
         title={
           <div className={styles.termsCardHeader}>
             <span className={styles.title}>
-              INCLUDE Portal Terms & Conditions
+              {advancedMsg("terms_title")}
             </span>
             <span className={styles.lastUpdateDate}>
-              Last Update: 07/11/2021
+              {LAST_UPDATED_DATE}
+              <Tooltip arrowPointAtCenter placement="top" title={advancedMsg("last_update_tooltip")} className={styles.tooltipGold}>
+                <InfoCircleOutlined className={styles.tooltipIcon} />
+              </Tooltip>
             </span>
           </div>
         }
         content={
           <ScrollContent className={styles.termsListWrapper}>
-            <ul className={styles.termsList}>
-              <li>
-                My purpose for the use of INCLUDE Portal data is free from
-                discrimination on the grounds of race, ethnicity, nationality,
-                gender, age, physical and/or mental ability, sexual orientation,
-                gender identity or expression, religion, or any other grounds
-                that would impinge on an individual’s rights.
-              </li>
-              <li>
-                I will acknowledge specific dataset(s) and/or applicable
-                accession number(s) as well as the INCLUDE Data Hub in my
-                dissemination of research findings, as applicable to the medium
-                or type of dissemination.
-              </li>
-              <li>
-                I will only share or distribute INCLUDE Portal data under terms
-                consistent with this agreement, and the data or derivatives of
-                the data may not be sold, in whole or in part, to any individual
-                at any point in time for any purpose.
-              </li>
-              <li>
-                I will respect the privacy of research participants, and I will
-                make no attempt to identify or contact individual participants
-                or groups from whom data were collected or to generate
-                information that could allow participants’ identities to be
-                readily ascertained.
-              </li>
-              <li>
-                I agree to provide a brief statement regarding my intended use
-                of the data on the INCLUDE Portal with my name and affiliation
-                which will be publicly displayed for the purpose of transparency
-                and collaboration.
-              </li>
-              <li>
-                I understand that participation in the INCLUDE community is
-                voluntary and may be terminated by the INCLUDE Portal
-                Administrator. I will report any actual or suspected violation
-                of this agreement, even if unintentional, to the INCLUDE Portal
-                Administrator. I understand that the INCLUDE Portal
-                Administrator may take action to remedy any actual or suspected
-                violation and/or report such behavior to the appropriate
-                authorities. I also understand that the INCLUDE Portal
-                Administrator may immediately suspend or terminate my access to
-                the INCLUDE Portal if there is an actual or suspected violation
-                of this agreement.
-              </li>
-            </ul>
+            <ol className={styles.termsList}>
+              <li>{advancedMsg("terms_1")}</li>
+              <li>{advancedMsg("terms_2")}</li>
+              <li>{advancedMsg("terms_3")}</li>
+              <li>{advancedMsg("terms_4")}</li>
+              <li>{advancedMsg("terms_5")}</li>
+              <li>{advancedMsg("terms_6")}</li>
+              <li>{advancedMsg("terms_7")}</li>
+            </ol>
           </ScrollContent>
         }
       />
       <GridCard
         wrapperClassName={styles.cardWrapper}
         className={styles.card}
-        title="INCLUDE Portal Disclaimers"
+        title={advancedMsg("disclaimer_title")}
         content={
           <ul className={styles.termsList}>
-            <li>
-              Data available in the INCLUDE Portal is provided on an AS-IS basis
-              and may change over time.
-            </li>
-            <li>
-              The INCLUDE DCC does not warrant or assume any legal liability or
-              responsibility for information, apparatus, product, or process
-              contained in the INCLUDE Portal.
-            </li>
-            <li>
-              Content provided on the INCLUDE Portal is for informational
-              purposes only and is not intended to be a substitute for
-              independent professional medical judgment, advice, diagnosis, or
-              treatment.
-            </li>
+            <li>{advancedMsg("disclaimer_1")}</li>
+            <li>{advancedMsg("disclaimer_2")}</li>
+            <li>{advancedMsg("disclaimer_3")}</li>
           </ul>
         }
       />
@@ -170,7 +127,7 @@ const TermsStep = ({
                 return value?.length === 2
                   ? Promise.resolve()
                   : Promise.reject(
-                      "Please accept the terms & conditions and portal disclaimers."
+                      advancedMsg("terms_disclaimer_error")
                     );
               },
               validateTrigger: "onSubmit",
@@ -180,23 +137,23 @@ const TermsStep = ({
           <Checkbox.Group>
             <Space direction="vertical">
               <Checkbox value={CHECKBOXES_OPTIONS.ACCEPTED_TERMS}>
-                I have read and agree to the INCLUDE Portal Terms and Conditions
+                {advancedMsg("terms_checkbox")}
               </Checkbox>
               <Checkbox value={CHECKBOXES_OPTIONS.UNDERSTAND_DISCLAIMER}>
-                I have read and understand the INCLUDE Portal Disclaimers
+              {advancedMsg("disclaimer_checkbox")}
               </Checkbox>
             </Space>
           </Checkbox.Group>
         </Form.Item>
       </Form>
       <Space className={styles.checkboxFormFooter}>
-        <Button onClick={() => {}}>Cancel</Button>
+        <Button onClick={() =>  window.location.href=redirectUrl}>{advancedMsg("cancel")}</Button>
         <Button
           type="primary"
           className="iconRight"
           onClick={() => form.submit()}
         >
-          Next <ArrowRightOutlined />
+          {advancedMsg("next")} <ArrowRightOutlined />
         </Button>
       </Space>
     </Space>
