bc4f14c8295b8db5f75fc52a7ed56a1399ca5242
Celine Pelletier <cpelletier@ferlab.bio>
2023-02-16 11:49:58 -0500

    feat: CQDG-38 Login page with CQDG styling and add lang switch

--- themes/cqdg/src/keycloak/kcMessagesExtension.ts
+++ themes/cqdg/src/keycloak/kcMessagesExtension.ts
@@ -4,8 +4,14 @@ import { kcMessages } from "keycloakify/lib/i18n/useKcMessage";
 Object.assign(
     kcMessages["en"],
     {
-        "foo": "foo in English",
-        "loginWith": "Log in with",
-        "era_commons_id": "ERA Commons ID"
+        "loginTitle": "Log in with",
+    }
+);
+
+
+Object.assign(
+    kcMessages["fr"],
+    {
+        "loginTitle": "Choisir votre identifiant",
     }
 );
